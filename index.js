f641bb457025278cd6dc71f62baab7c8c0139b61
Suched Chaisiricharoenkul <nok_044@hotmail.com>
2019-02-10 14:03:43 +0700

    calculate path

--- index.js
+++ index.js
@@ -2,5 +2,93 @@ const lib = require('./lib')
 const Graph = lib.Graph
 
 var graph = new Graph()
-graph.addNode('A','B',1)
-graph.addNode('A','B',2)
+
+graph
+    .addNode('A', 'B', 1)
+    .addNode('A', 'C', 4)
+    .addNode('A', 'D', 10)
+    .addNode('B', 'E', 3)
+    .addNode('C', 'D', 4)
+    .addNode('C', 'F', 2)
+    .addNode('D', 'E', 1)
+    .addNode('E', 'B', 3)
+    .addNode('E', 'A', 2)
+    .addNode('F', 'D', 1)
+
+console.log('the​ ​delivery​ ​cost​ ​for​ ​route​ ​A-B-E : %s', (() => {
+    try{
+        return graph.specifiedPaths(['A','B','E'])
+    }catch (e) {
+        return e.message
+    }
+})())
+console.log('the​ ​delivery​ ​cost​ ​for​ ​route​ ​A-D : %s', (() => {
+    try{
+        return graph.specifiedPaths(['A','D'])
+    }catch (e) {
+        return e.message
+    }
+})())
+console.log('the​ ​delivery​ ​cost​ ​for​ ​route​ ​​​E-A-C-F : %s', (() => {
+    try{
+        return graph.specifiedPaths(['E','A','C','F'])
+    }catch (e) {
+        return e.message
+    }
+})())
+console.log('the​ ​delivery​ ​cost​ ​for​ ​route​ ​A-D-F : %s', (() => {
+    try{
+        return graph.specifiedPaths(['A','D','F'])
+    }catch (e) {
+        return e.message
+    }
+})())
+
+console.log('The​ ​number​ ​of​ ​possible​ ​delivery​ ​route​ ​from​ ​E​ ​to​ ​D​ ​with​ ​a​ ​maximum​ ​of​ ​4​ ​stop without​ ​using​ ​the​ ​same​ ​route​ ​twice​ ​in​ ​a​ ​delivery​ ​route : %s', (() => {
+    try{
+        return graph.possiblePaths('E','D', {
+            allowLoop: true,
+            maxStop: 4
+        }).length
+    }catch (e) {
+        return e.message
+    }
+})())
+
+console.log('The​ ​number​ ​of​ ​possible​ ​delivery​ ​route​ ​from​ ​E​ ​to​ ​E​ ​without​ ​using​ ​the​ ​same route​ ​twice​ ​in​ ​a​ ​delivery​ ​route : %s', (() => {
+    try{
+        return graph.possiblePaths('E','E', {
+            allowLoop: true
+        }).length
+    }catch (e) {
+        return e.message
+    }
+})())
+
+console.log('Bonus​:​ ​the​ ​number​ ​of​ ​possible​ ​delivery​ ​route​ ​from​ ​E​ ​to​ ​E​ ​that​ ​delivery​ ​cost​ ​is less​ ​than​ ​20.​ ​Given​ ​that​ ​the​ ​same​ ​route​ ​can​ ​be​ ​used​ ​twice​ ​in​ ​a​ ​delivery​ ​route : %s', (() => {
+    try{
+        return graph.possiblePaths('E','E', {
+            allowLoop: true,
+            allowSameRoute: true,
+            maxCost: 19
+        }).length
+    }catch (e) {
+        return e.message
+    }
+})())
+
+console.log('The​ ​cost​ ​of​ ​cheapest​ ​delivery​ ​route​ ​between​ ​E​ ​to​ ​D : %s', (() => {
+    try{
+        return graph.shortestPaths('E','D').cost
+    }catch (e) {
+        return e.message
+    }
+})())
+
+console.log('The​ ​cost​ ​of​ ​cheapest​ ​delivery​ ​route​ ​between​ ​E​ ​to​ ​E : %s', (() => {
+    try{
+        return graph.shortestPaths('E','E').cost
+    }catch (e) {
+        return e.message
+    }
+})())
--- test/graph/graph.test.js
+++ test/graph/graph.test.js
@@ -34,7 +34,7 @@ describe('Graph', () => {
 
             graph.addNode('A', 'B', 1)
 
-            let node = graph.graph.filter(n => n.name === 'A').pop()
+            let node = graph.paths.filter(n => n.name === 'A').pop()
 
             assert.equal(node.name,'A')
             assert.equal(node.edge.length,1)
@@ -47,7 +47,7 @@ describe('Graph', () => {
 
             graph.addNode('A', 'B', 1)
 
-            let node = graph.graph.filter(n => n.name === 'A').pop()
+            let node = graph.paths.filter(n => n.name === 'A').pop()
 
             assert.equal(node.name,'A')
             assert.equal(node.edge.length,1)
@@ -62,7 +62,7 @@ describe('Graph', () => {
 
             graph.addNode('A', 'B', 1)
 
-            let node = graph.graph.filter(n => n.name === 'A').pop()
+            let node = graph.paths.filter(n => n.name === 'A').pop()
 
             assert.equal(node.name,'A')
             assert.equal(node.edge.length,1)
@@ -71,7 +71,7 @@ describe('Graph', () => {
 
             graph.addNode('B', 'C', 2)
 
-            node = graph.graph.filter(n => n.name === 'B').pop()
+            node = graph.paths.filter(n => n.name === 'B').pop()
 
             assert.equal(node.name,'B')
             assert.equal(node.edge.length,1)
@@ -84,7 +84,7 @@ describe('Graph', () => {
 
             graph.addNode('A', 'B', 1)
 
-            let node = graph.graph.filter(n => n.name === 'A').pop()
+            let node = graph.paths.filter(n => n.name === 'A').pop()
 
             assert.equal(node.name,'A')
             assert.equal(node.edge.length,1)
@@ -98,6 +98,225 @@ describe('Graph', () => {
             let filter = node.edge.filter(n => n.name === 'C');
             assert.equal(filter.length,1)
             assert.equal(filter[0].cost,2)
+        })
+    })
+
+    describe('#specifiedPaths()', () => {
+        it('Invalid paths type', () => {
+            const graph = new Graph()
+            expect(() => graph.specifiedPaths('A')).toThrow('Invalid param(s)')
+        })
+
+        it('Invalid paths size', () => {
+            const graph = new Graph()
+
+            expect(() => graph.specifiedPaths(['A'])).toThrow('Paths must​ ​choose​ ​at​ ​least​ ​2​ node')
+        })
+
+        it('Invalid paths', () => {
+            const graph = new Graph()
+
+            expect(() => graph.specifiedPaths(['A','B'])).toThrow('No​ ​Such​ ​Route')
+        })
+
+        it('Find cost of 2 nodes', () => {
+            const graph = new Graph()
+
+            graph.addNode('A', 'B', 1)
+
+            let cost = graph.specifiedPaths(['A','B'])
+            assert.equal(cost, 1)
+        })
+
+        it('Find cost of 3 nodes', () => {
+            const graph = new Graph()
+
+            graph
+                .addNode('A', 'B', 1)
+                .addNode('B', 'C', 2)
+
+            let cost = graph.specifiedPaths(['A','B','C'])
+            assert.equal(cost, 3)
+        })
+
+        it('Invalid paths with data', () => {
+            const graph = new Graph()
+
+            graph
+                .addNode('A', 'B', 1)
+                .addNode('B', 'C', 2)
+
+            expect(() => graph.specifiedPaths(['A','C'])).toThrow('No​ ​Such​ ​Route')
+        })
+    })
+
+    describe('#possiblePaths()', () => {
+        it('Invalid start type', () => {
+            const graph = new Graph()
+            expect(() => graph.possiblePaths(1,'B',{})).toThrow('Invalid param(s)')
+        })
+
+        it('Invalid end type', () => {
+            const graph = new Graph()
+            expect(() => graph.possiblePaths('A',1,{})).toThrow('Invalid param(s)')
+        })
+
+        it('Invalid condition type', () => {
+            const graph = new Graph()
+            expect(() => graph.possiblePaths('A','B',1)).toThrow('Invalid param(s)')
+        })
+
+        it('Invalid paths', () => {
+            const graph = new Graph()
+
+            expect(() => graph.possiblePaths('A','B')).toThrow('No​ ​Such​ ​Route')
+        })
+
+        it('Find possible path of 2 nodes', () => {
+            const graph = new Graph()
+
+            graph.addNode('A', 'B', 1)
+
+            let paths = graph.possiblePaths('A','B')
+            assert.equal(paths.length, 1)
+            assert.equal(paths[0][0], 'A')
+            assert.equal(paths[0][1], 'B')
+        })
+
+        it('Find possible path of 3 nodes', () => {
+            const graph = new Graph()
+
+            graph.addNode('A', 'B', 1)
+                .addNode('B', 'C', 2)
+
+            let paths = graph.possiblePaths('A','C')
+            assert.equal(paths.length, 1)
+            assert.equal(paths[0][0], 'A')
+            assert.equal(paths[0][1], 'B')
+            assert.equal(paths[0][2], 'C')
+        })
+
+        it('Find possible path of 3 nodes with multiple path', () => {
+            const graph = new Graph()
+
+            graph.addNode('A', 'B', 1)
+                .addNode('B', 'C', 2)
+                .addNode('A', 'C', 2)
+
+            let paths = graph.possiblePaths('A','C')
+            assert.equal(paths.length, 2)
+        })
+
+        it('Find possible path of 3 nodes with allowLoop', () => {
+            const graph = new Graph()
+
+            graph.addNode('A', 'B', 1)
+                .addNode('B', 'C', 2)
+                .addNode('A', 'C', 2)
+
+            let paths = graph.possiblePaths('A','C', {
+                allowLoop: true
+            })
+            assert.equal(paths.length, 2)
+        })
+
+        it('Find possible path of 3 nodes with maxStop', () => {
+            const graph = new Graph()
+
+            graph.addNode('A', 'B', 1)
+                .addNode('B', 'C', 2)
+                .addNode('A', 'C', 2)
+
+            let paths = graph.possiblePaths('A','C', {
+                maxStop: 1
+            })
+            assert.equal(paths.length, 1)
+        })
+
+        it('Find possible path of 3 nodes with allowSameRoute', () => {
+            const graph = new Graph()
+
+            graph.addNode('A', 'B', 1)
+                .addNode('B', 'C', 2)
+                .addNode('A', 'C', 2)
+
+            let paths = graph.possiblePaths('A','C', {
+                allowSameRoute: true
+            })
+            assert.equal(paths.length, 2)
+        })
+
+        it('Find possible path of 3 nodes with maxCost', () => {
+            const graph = new Graph()
+
+            graph.addNode('A', 'B', 1)
+                .addNode('B', 'C', 2)
+                .addNode('A', 'C', 2)
+
+            let paths = graph.possiblePaths('A','C', {
+                maxCost: 2
+            })
+            assert.equal(paths.length, 1)
+        })
+    })
+
+    describe('#shortestPaths()', () => {
+        it('Invalid start type', () => {
+            const graph = new Graph()
+            expect(() => graph.shortestPaths(1,'B',{})).toThrow('Invalid param(s)')
+        })
+
+        it('Invalid end type', () => {
+            const graph = new Graph()
+            expect(() => graph.shortestPaths('A',1,{})).toThrow('Invalid param(s)')
+        })
+
+        it('Invalid condition type', () => {
+            const graph = new Graph()
+            expect(() => graph.shortestPaths('A','B',1)).toThrow('Invalid param(s)')
+        })
+
+        it('Invalid paths', () => {
+            const graph = new Graph()
+
+            expect(() => graph.shortestPaths('A','B')).toThrow('No​ ​Such​ ​Route')
+        })
+
+        it('Find shortest path of 2 nodes', () => {
+            const graph = new Graph()
+
+            graph.addNode('A', 'B', 1)
+
+            let paths = graph.shortestPaths('A','B')
+            assert.equal(paths.cost, 1)
+            assert.equal(paths.path[0], 'A')
+            assert.equal(paths.path[1], 'B')
+        })
+
+        it('Find shortest path of 3 nodes', () => {
+            const graph = new Graph()
+
+            graph.addNode('A', 'B', 1)
+                .addNode('B', 'C', 2)
+
+            let paths = graph.shortestPaths('A','C')
+            assert.equal(paths.cost, 3)
+            assert.equal(paths.path[0], 'A')
+            assert.equal(paths.path[1], 'B')
+            assert.equal(paths.path[2], 'C')
+        })
+
+        it('Find shortest path of 3 nodes with multiple path', () => {
+            const graph = new Graph()
+
+            graph.addNode('A', 'B', 1)
+                .addNode('B', 'C', 2)
+                .addNode('A', 'C', 2)
+
+            let paths = graph.shortestPaths('A','C')
+            assert.equal(paths.cost, 2)
+            assert.equal(paths.path[0], 'A')
+            assert.equal(paths.path[1], 'C')
         })
     })
 })
